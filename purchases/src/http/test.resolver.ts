39210e428d211910ee3f374d66bf5810de9ab8aa
Lucas Viga <lucasviga12@gmail.com>
2022-04-10 17:36:54 -0400

    lesson 01 - basic structure of all the projects

---
+++ purchases/src/http/test.resolver.ts
@@ -0,0 +1,16 @@
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query } from '@nestjs/graphql';
+
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+
+@Resolver('test')
+export class TestResolver {
+  constructor(private prisma: PrismaService) {}
+
+  @Query(() => String)
+  @UseGuards(AuthorizationGuard)
+  hello() {
+    return 'Hello World';
+  }
+}
